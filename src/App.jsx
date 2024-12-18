b0f1d3f16494c67495ee150c676ce87098c40142
novaldymlb <139160476+novaldymlb@users.noreply.github.com>
2024-12-18 18:41:20 +0900

    Atur gaya gulir halaman

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import './app.scss';
+import Navbar from './components/navbar/Navbar';
+
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <section>
+        <Navbar />
+        Hero
+        <div>
+          <a href=''>Link</a>
+        </div>
+      </section>
+      <section>Timeline</section>
+      <section>Skill</section>
+      <section>Project Parallax</section>
+      <section>Project 1</section>
+      <section>Project 2</section>
+      <section>Project 3</section>
+      <section>Project 4</section>
+      <section>Contact</section>
+    </div>
+  );
 };
 
 export default App;
